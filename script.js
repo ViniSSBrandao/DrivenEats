453331e262d151eeb7c9434ab8f249e955941131
teuprimo <vinicius.brandaot@gmail.com>
2022-10-15 23:31:00 -0300

    selecao de itens sem limite

---
+++ script.js
@@ -0,0 +1,23 @@
+function select_p(opt){
+   
+    
+   opt.classList.toggle('teste');
+   console.log(opt);
+
+}
+
+function select_b(opt){
+   
+    
+   opt.classList.toggle('teste');
+   console.log(opt);
+
+}
+
+function select_s(opt){
+   
+    
+   opt.classList.toggle('teste');
+   console.log(opt);
+
+}
