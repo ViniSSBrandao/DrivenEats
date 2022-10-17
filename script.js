1e464206630a20dd7f0afa54acf780a2aa549515
teuprimo <vinicius.brandaot@gmail.com>
2022-10-17 15:34:19 -0300

    check responsivo

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //selecionar prato principal//
 let prato_sel = null, bebida_sel = null, sobremesa_sel = null;
-const preco_sapato = 35.99, preco_gato = 24.95, preco_suco=6.99, preco_abacate=4.99; //valor dos pratos disponíveis
+
 let pratos;
 let valor_p, valor_b, valor_s; //valor dos pratos selecionados//
 let coms, bebs, doce;
+let check;
 
 function select_p(opt_p){
    
    
    if(prato_sel !== null){
       prato_sel.classList.remove('teste');
-      
+      check_b.classList.add('hide')
       
    }
    
@@ -20,6 +21,11 @@ function select_p(opt_p){
    console.log(opt_p);
    prato_sel = opt_p;
    
+   
+   check = prato_sel.querySelector('.hide');
+   check.classList.remove('hide');
+   check_b = check;
+
    valor_p = prato_sel.querySelector('#preco').innerHTML;
    coms = prato_sel.querySelector('#nome').innerHTML;
    valor_p = parseFloat(valor_p.replace(',' , '.'))
@@ -39,18 +45,22 @@ function select_b(opt_b){
 
    if(bebida_sel !== null){
       bebida_sel.classList.remove('teste');
-     
+     check.classList.add('hide')
      
    } 
    
    opt_b.classList.toggle('teste');
-   console.log(opt_b);
    bebida_sel = opt_b;
    valor_b = bebida_sel.querySelector('#preco').innerHTML;
    bebs = bebida_sel.querySelector('#nome').innerHTML;
-   valor_b = parseFloat(valor_b.replace(',' , '.'))
+   
+   check = bebida_sel.querySelector('.hide');
+   check.classList.remove('hide');
+   check_b = check;
+
+   valor_b = parseFloat(valor_b.replace(',' , '.'));
    console.log(valor_b);
-   console.log(preco_b_f)
+   
    realizar_pedido();
    return bebida_sel;
  
@@ -64,13 +74,18 @@ function select_s(opt_s){
 
    if(sobremesa_sel !== null){
       sobremesa_sel.classList.remove('teste')
-      
+      check_b.classList.add('hide')
   
    }
    
    opt_s.classList.toggle('teste');
    sobremesa_sel = opt_s;
-   console.log(opt_s);
+   
+   check = sobremesa_sel.querySelector('.hide');
+   check.classList.remove('hide');
+   check_b = check;
+
+
    valor_s = sobremesa_sel.querySelector('#preco').innerHTML;
    doce = sobremesa_sel.querySelector('#nome').innerHTML;
    valor_s = parseFloat(valor_s.replace(',' , '.'))
@@ -85,12 +100,12 @@ function select_s(opt_s){
 function realizar_pedido(){
    console.log('pedido atualizado')
    if((sobremesa_sel&&prato_sel&&bebida_sel)!==null){
-      element = document.querySelector('.order');
-      element.classList.add('confirm');
-      element.innerHTML = "Fechar pedido";
-      soma = valor_b+valor_p+valor_s
-      soma = soma.toFixed(2)
-      pratos = `Gostaria de fazer um pedido: \n- Prato: ${coms} \n- Bebida: ${bebs}\n- sobremesa: ${doce} \nTotal: R$${soma}`;
+         element = document.querySelector('.order');
+         element.classList.add('confirm');
+         element.innerHTML = "Fechar pedido";
+         soma = valor_b+valor_p+valor_s
+         soma = soma.toFixed(2)
+         pratos = `Gostaria de fazer um pedido: \n- Prato: ${coms} \n- Bebida: ${bebs}\n- sobremesa: ${doce} \nTotal: R$${soma}`;
    }
 }
 
@@ -98,7 +113,7 @@ function message(nhu){
    
    console.log(pratos);
    if((sobremesa_sel&&prato_sel&&bebida_sel)!==null){
-   window.open(encodeURI("https://wa.me/?text="+pratos));
+      window.open(encodeURI("https://wa.me/?text="+pratos));
    }
 }
 
